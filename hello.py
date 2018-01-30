268e47711aa04a1187902c41273b992bb5b19415
zhikunzhu <zz1u17@soton.ac.uk>
2018-01-30 16:36:36 +0000

    30/Jan

--- hello.py
+++ hello.py
@@ -1,4 +1,20 @@
 
 a = 1;
+b = True;
+# string
+c = "{name} wants to eat {food}".format(name="Bob", food="lasagna");
+d = "%s can be %s" % ("strings", "interpolated");
 
-print a
+if b:
+    print 'I love python'
+print c
+d = [x + 3 for x in range(6)]
+print d
+
+def f(a,L = [1,2]):
+    L.append(a)
+    return L
+
+print(f(1))
+print(f(2))
+print(f(3))
